9507f853501383bbbb2a55b7cdce60aa90c160b2
Stephan Vedder <stephan.vedder@gmail.com>
2015-03-06 18:45:33 +0100

    Added Writi string functions

--- export_w3d.py
+++ export_w3d.py
@@ -10,5 +10,20 @@ from mathutils import Vector, Matrix, Quaternion
 from . import struct_w3d
 
 
+def WriteString(string, file):
+	#TODO: check if it does write nullterminated strings
+   	file.write(string)
+    return
+
+def WriteFixedString(string,file):
+	#truncate the string to 16
+	nullbytes = 16-len(string)
+	if(nullbytes<0)
+		print("Warning: Fixed string is too long")
+
+	file.write(string)
+	for i in xrange(nullbytes)
+		file.write(struct.pack("c",'\0'))
+
 def MainExport(givenfilepath, self, context):
 	print("Run Export")
